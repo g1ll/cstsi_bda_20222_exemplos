0ae573da1535fa696f9dee2554bb2c20ee127db3
Gill Velleda Gonzales <gillvelleda@gmail.com>
2022-08-17 15:09:05 -0300

    feat: mongodb insert documents

---
+++ aula06/app.js
@@ -0,0 +1,66 @@
+import { MongoClient } from "mongodb";
+import * as fs from 'node:fs/promises';
+
+const myDB = {
+	domain: "localhost",
+	port: 27017
+}
+
+const uri = `mongodb://${myDB.domain}:${myDB.port}`
+const client = new MongoClient(uri)
+
+try {
+	await client.connect();
+	await client.db('admin').command({ 'ping': 1 })
+	console.log('Conectado ao mongo!')
+
+	// const produto = {
+	// 	id_prod: 157,
+	// 	nome: "Novo Produto Teste TRES",
+	// 	descricao:"Testando inserção de novo produto"
+	// }
+
+	// const result = await client.db('loja')
+	// 			.collection('produtos')
+	// 			.insertOne(produto)
+
+	// result.acknowledged && console.log("Produto inserido!!")
+
+	//Insert Many
+
+	// const produtos = [
+	// 	{
+	// 		id_prod: 158,
+	// 		nome: "Novo Produto Teste quatro",
+	// 		descricao: "Testando inserção de novo produto"
+	// 	},
+	// 	{
+	// 		id_prod: 159,
+	// 		nome: "Novo Produto Teste quinto",
+	// 		descricao: "Testando inserção de novo produto"
+	// 	},
+	// 	{
+	// 		id_prod: 160,
+	// 		nome: "Novo Produto Teste sexto",
+	// 		descricao: "Testando inserção de novo produto"
+	// 	}
+	// ]
+
+	const jsonFile = await fs.readFile('./produtos.json')
+	const produtos = JSON.parse(jsonFile);
+	console.log(produtos)
+
+	const result = await client.db('mercadinho')
+				.collection('produtos')
+				.insertMany(produtos)
+
+	console.log(result)
+
+
+} catch (error) {
+	console.log("ERROR")
+	console.log(error)
+}
+finally {
+	process.exit(0)
+}
